04af87aa88759d276196f3f45a1772c4f2097639
un-ast <unast.eth@gmail.com>
2023-03-11 16:30:18 +0900

    test: Pool#new

--- tests/Pool.spec.ts
+++ tests/Pool.spec.ts
@@ -1,3 +1,4 @@
+import { encodeAddress } from '@polkadot/keyring'
 import { deployPool } from './testContractsHelper'
 import { zeroAddress } from './testHelpers'
 
@@ -5,17 +6,22 @@ describe('Pool spec', () => {
   const setup = async () => {
     const { api, alice: deployer } = globalThis.setup
 
+    const tokenAddr = encodeAddress(
+      '0x0000000000000000000000000000000000000000000000000000000000000001',
+    )
+
     const pool = await deployPool({
       api,
       signer: deployer,
-      args: [],
+      args: [tokenAddr],
     })
 
-    return pool
+    return { pool, tokenAddr }
   }
 
   it('instantiate', async () => {
-    const pool = await setup()
+    const { pool, tokenAddr } = await setup()
     expect(pool.address).not.toBe(zeroAddress)
+    expect((await pool.query.underlying()).value.ok).toEqual(tokenAddr)
   })
 })
