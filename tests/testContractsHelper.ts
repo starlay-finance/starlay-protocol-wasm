69b542e793d538b8f953c51ffc2173b1e42b5228
un-ast <unast.eth@gmail.com>
2023-03-12 13:01:26 +0900

    test: e2e controller

--- tests/testContractsHelper.ts
+++ tests/testContractsHelper.ts
@@ -1,15 +1,29 @@
 import type { ApiPromise } from '@polkadot/api'
 import type { KeyringPair } from '@polkadot/keyring/types'
 
+import Controller_factory from '../types/constructors/controller'
 import Pool_factory from '../types/constructors/pool'
 import PSP22Token_factory from '../types/constructors/psp22_token'
 
+import Controller from '../types/contracts/controller'
 import Pool from '../types/contracts/pool'
 import PSP22Token from '../types/contracts/psp22_token'
 
 type FactoryArgs = {
   api: ApiPromise
   signer: KeyringPair
+}
+
+export const deployController = async ({
+  api,
+  signer,
+  args,
+}: FactoryArgs & {
+  args: Parameters<Controller_factory['new']>
+}): Promise<Controller> => {
+  const factory = new Controller_factory(api, signer)
+  const contract = await factory.new(...args)
+  return new Controller(contract.address, signer, api)
 }
 
 export const deployPool = async ({
