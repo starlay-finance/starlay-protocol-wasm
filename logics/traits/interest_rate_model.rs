ff425ff4aa56b8b16456b57bccb0bf0b10964dd3
un-ast <unast.eth@gmail.com>
2023-04-01 16:16:54 +0900

    clean: docs - manager/price_oracle/interest_rate traits

--- logics/traits/interest_rate_model.rs
+++ logics/traits/interest_rate_model.rs
@@ -5,11 +5,14 @@ use super::types::WrappedU256;
 #[openbrush::wrapper]
 pub type InterestRateModelRef = dyn InterestRateModel;
 
+/// Trait defines the interface for interest rate model
 #[openbrush::trait_definition]
 pub trait InterestRateModel {
+    /// Calculates the current borrow interest rate per milliseconds
     #[ink(message)]
     fn get_borrow_rate(&self, cash: Balance, borrows: Balance, reserves: Balance) -> WrappedU256;
 
+    /// Calculates the current supply interest rate per milliseconds
     #[ink(message)]
     fn get_supply_rate(
         &self,
--- logics/traits/price_oracle.rs
+++ logics/traits/price_oracle.rs
@@ -3,12 +3,19 @@ use openbrush::traits::AccountId;
 #[openbrush::wrapper]
 pub type PriceOracleRef = dyn PriceOracle;
 
+/// Trait defines the functions that a PriceOracle must implement.
+/// A PriceOracle is responsible for providing the current market price of an asset.
 #[openbrush::trait_definition]
 pub trait PriceOracle {
+    /// Returns the current price for the given asset, if available.
     #[ink(message)]
     fn get_price(&self, asset: AccountId) -> Option<u128>;
+
+    /// Returns the underlying price of the given pool, if available.
     #[ink(message)]
-    fn get_underlying_price(&self, asset: AccountId) -> Option<u128>;
+    fn get_underlying_price(&self, pool: AccountId) -> Option<u128>;
+
+    /// Sets a fixed price for the given asset.
     #[ink(message)]
     fn set_fixed_price(&mut self, asset: AccountId, value: u128) -> Result<()>;
 }
