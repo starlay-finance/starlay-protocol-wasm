a4722eaee6b8ffbf2d0b72ac52b49cfee3fcf9cf
Wolf <wolfwarrier14@gmail.com>
2023-07-31 02:35:11 -0500

    update dependancies

--- tests/testHelpers.ts
+++ tests/testHelpers.ts
@@ -1,6 +1,5 @@
 import { ReturnNumber } from '@727-ventures/typechain-types'
-import type { WeightV2 } from '@polkadot/types/interfaces'
-import { BN, BN_ONE } from '@polkadot/util'
+import { BN } from '@polkadot/util'
 import { ReplacedType } from '../scripts/helper/utilityTypes'
 import { waitForTx } from '../scripts/helper/utils'
 
@@ -25,40 +24,6 @@ export const expectToEmit = <T = unknown>(
   })
 }
 
-const generateNewArgsWithGasLimit = <
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  C extends { tx: any; query: any; name: string },
-  F extends keyof C['tx'],
->(
-  contract: C,
-  fn: F,
-  args: Parameters<C['tx'][F]>,
-  gasLimit: WeightV2,
-  storageDepositLimit: WeightV2 | null,
-): Parameters<C['tx'][F]> => {
-  if (args.length == contract.tx[fn].length) {
-    const option: object = args[args.length - 1] as object
-    return [
-      ...args.slice(0, args.length - 1),
-      {
-        ...option,
-        gasLimit,
-        storageDepositLimit,
-      },
-    ] as Parameters<C['tx'][F]>
-  } else {
-    return [
-      ...args,
-      {
-        gasLimit,
-        storageDepositLimit,
-      },
-    ] as Parameters<C['tx'][F]>
-  }
-}
-
-const MAX_CALL_WEIGHT = new BN(5_000_000_000_000_000).isub(BN_ONE)
-const PROOFSIZE = new BN(1_000_000_000)
 export const shouldNotRevert = async <
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   C extends { tx: any; query: any; name: string },
@@ -69,23 +34,9 @@ export const shouldNotRevert = async <
   fn: F,
   args: Parameters<C['tx'][F]>,
 ): Promise<R> => {
-  let gasRequired: WeightV2
-  const { api } = globalThis.setup
   try {
-    const gasLimit = api?.registry.createType('WeightV2', {
-      refTime: MAX_CALL_WEIGHT,
-      proofSize: PROOFSIZE,
-    }) as WeightV2
-    const newArgs = generateNewArgsWithGasLimit(
-      contract,
-      fn,
-      args,
-      gasLimit,
-      null,
-    )
-    const preview = await contract.query[fn](...newArgs)
+    const preview = await contract.query[fn](...args)
     expect(preview.value.ok.err).toBeUndefined()
-    gasRequired = preview.gasRequired as WeightV2
   } catch (e) {
     throw new Error(
       `failed to preview ${contract.name}.${fn as string}(${JSON.stringify(
@@ -93,17 +44,8 @@ export const shouldNotRevert = async <
       )}): ${JSON.stringify(e)}`,
     )
   }
-  // console.log(fn, 'gasRequired.refTime', gasRequired.refTime.toString())
-  // console.log(fn, 'gasRequired.proofSize', gasRequired.proofSize.toString())
-  const gasLimit = api?.registry.createType('WeightV2', gasRequired) as WeightV2
-  const newArgs = generateNewArgsWithGasLimit(
-    contract,
-    fn,
-    args,
-    gasLimit,
-    null,
-  )
-  const res = await contract.tx[fn](...newArgs)
+
+  const res = await contract.tx[fn](...args)
   await waitForTx(res)
   return res
 }
