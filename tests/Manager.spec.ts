350bdcd0ae0f9cd6c4262568233dd650c8b5169a
Wolf <wolfwarrior0514@gmail.com>
2024-02-06 00:34:21 -0600

    make test for transfer manager for controller and pool

--- tests/Manager.spec.ts
+++ tests/Manager.spec.ts
@@ -1,4 +1,6 @@
+import type { ApiPromise } from '@polkadot/api'
 import { encodeAddress } from '@polkadot/keyring'
+import type { KeyringPair } from '@polkadot/keyring/types'
 import { BN } from '@polkadot/util'
 import { ONE_ETHER, ROLE, ZERO_ADDRESS } from '../scripts/helper/constants'
 import {
@@ -8,7 +10,14 @@ import {
   deployManager,
   deployPriceOracle,
 } from '../scripts/helper/deploy_helper'
-import { preparePoolsWithPreparedTokens } from './testContractHelper'
+import Controller from '../types/contracts/controller'
+import Manager from '../types/contracts/manager'
+import PriceOracle from '../types/contracts/price_oracle'
+import {
+  PoolContracts,
+  Pools,
+  preparePoolsWithPreparedTokens,
+} from './testContractHelper'
 import { shouldNotRevert } from './testHelpers'
 
 describe('Manager spec', () => {
@@ -48,7 +57,7 @@ describe('Manager spec', () => {
       controller,
       rateModel,
       signer: deployer,
-      manager: deployer.address,
+      manager: manager.address,
       incentivesController,
     })
 
@@ -58,7 +67,7 @@ describe('Manager spec', () => {
       args: [],
     })
 
-    return { deployer, manager, controller, pools, priceOracle }
+    return { deployer, manager, controller, pools, priceOracle, api }
   }
 
   it('instantiate', async () => {
@@ -155,6 +164,156 @@ describe('Manager spec', () => {
         poolAddr,
       )
       expect(value2.ok).toEqual(true)
+    })
+  })
+
+  describe('Transfer Controller Manager', () => {
+    let manager: Manager
+    let newManager: Manager
+    let controller: Controller
+    let deployer: KeyringPair
+    let api: ApiPromise
+
+    beforeAll(async () => {
+      ;({ manager, controller, api, deployer } = await setup())
+
+      await shouldNotRevert(manager, 'grantRole', [
+        ROLE.CONTROLLER_ADMIN,
+        deployer.address,
+      ])
+
+      newManager = await deployManager({
+        api,
+        signer: deployer,
+        args: [controller.address],
+      })
+
+      await shouldNotRevert(newManager, 'grantRole', [
+        ROLE.CONTROLLER_ADMIN,
+        deployer.address,
+      ])
+    })
+
+    it('Accept Controller Manager Failed. (Pending manager is not set)', async () => {
+      const result = (await newManager.query.acceptControllerManager()).value.ok
+      expect(result.err).toStrictEqual({ controller: 'PendingManagerIsNotSet' })
+    })
+
+    it('Set Controller Manager.', async () => {
+      await shouldNotRevert(manager, 'setControllerManager', [
+        newManager.address,
+      ])
+
+      const pendingManager = (await controller.query.pendingManager()).value.ok
+      expect(pendingManager).toBe(newManager.address)
+    })
+
+    it('Accept Controller Manager Failed. (Caller is not pending manager)', async () => {
+      const result = (await manager.query.acceptControllerManager()).value.ok
+      expect(result.err).toStrictEqual({
+        controller: 'CallerIsNotPendingManager',
+      })
+    })
+
+    it('Accept Controller Manager Works.', async () => {
+      await shouldNotRevert(newManager, 'acceptControllerManager', [])
+
+      const pendingManager = (await controller.query.pendingManager()).value.ok
+      expect(pendingManager).toBe(null)
+
+      const manager = (await controller.query.manager()).value.ok
+      expect(manager).toBe(newManager.address)
+    })
+  })
+
+  describe('Transfer Pool Manager', () => {
+    let manager: Manager
+    let newManager: Manager
+    let controller: Controller
+    let deployer: KeyringPair
+    let api: ApiPromise
+    let pools: Pools
+    let dai: PoolContracts
+    let priceOracle: PriceOracle
+
+    beforeAll(async () => {
+      ;({ manager, controller, api, deployer, pools, priceOracle } =
+        await setup())
+      ;({ dai } = pools)
+
+      await shouldNotRevert(manager, 'grantRole', [
+        ROLE.TOKEN_ADMIN,
+        deployer.address,
+      ])
+
+      await shouldNotRevert(manager, 'grantRole', [
+        ROLE.CONTROLLER_ADMIN,
+        deployer.address,
+      ])
+
+      await shouldNotRevert(manager, 'setPriceOracle', [priceOracle.address])
+
+      await shouldNotRevert(priceOracle, 'setFixedPrice', [
+        dai.token.address,
+        ONE_ETHER,
+      ])
+
+      const collateralFactor = ONE_ETHER.mul(new BN(90)).div(new BN(100))
+      await shouldNotRevert(
+        manager,
+        'supportMarketWithCollateralFactorMantissa',
+        [dai.pool.address, dai.token.address, [collateralFactor]],
+      )
+
+      newManager = await deployManager({
+        api,
+        signer: deployer,
+        args: [controller.address],
+      })
+
+      await shouldNotRevert(newManager, 'grantRole', [
+        ROLE.TOKEN_ADMIN,
+        deployer.address,
+      ])
+    })
+
+    it('Accept Pool Manager Failed. (Pending manager is not set)', async () => {
+      const result = (
+        await newManager.query.acceptPoolManager(dai.pool.address)
+      ).value.ok
+      expect(result.err).toStrictEqual({
+        pool: {
+          pendingManagerIsNotSet: null,
+        },
+      })
+    })
+
+    it('Set Pool Manager.', async () => {
+      await shouldNotRevert(manager, 'setPoolManager', [
+        dai.pool.address,
+        newManager.address,
+      ])
+
+      const pendingManager = (await dai.pool.query.pendingManager()).value.ok
+      expect(pendingManager).toBe(newManager.address)
+    })
+
+    it('Accept Pool Manager Failed. (Caller is not pending manager)', async () => {
+      const result = (await manager.query.acceptPoolManager(dai.pool.address))
+        .value.ok
+      expect(result.err).toStrictEqual({
+        pool: { callerIsNotPendingManager: null },
+      })
+    })
+
+    it('Accept Pool Manager Works.', async () => {
+      await shouldNotRevert(newManager, 'acceptPoolManager', [dai.pool.address])
+
+      const pendingManager = (await dai.pool.query.pendingManager()).value.ok
+      expect(pendingManager).toBe(null)
+
+      const manager = (await dai.pool.query.manager()).value.ok
+      expect(manager).toBe(newManager.address)
     })
   })
 })
